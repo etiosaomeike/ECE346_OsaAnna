8e1cacbafdeffa69c43410fbaeefdd4156718ce3
J-Etiosa-Omeike <jomeike@princeton.edu>
2023-05-05 19:14:25 -0400

    task 1 obstacles doesn't work

--- ROS_Core/src/Labs/FinalProject/scripts/task1_traj_planner.py
+++ ROS_Core/src/Labs/FinalProject/scripts/task1_traj_planner.py
@@ -7,6 +7,7 @@ from task2_world.util import RefPath
 import yaml
 from nav_msgs.msg import Odometry
 from nav_msgs.msg import Path as PathMsg
+import threading
 
 # Pull in all needed info/services as descirbed in readme file
 
@@ -14,11 +15,15 @@ class Task1_Controller:
     def __init__(self):
         
         print("The Task1 Controller Has Been Initialized")
-        self.list_of_goals, self.indexes_of_goals = self.parse_yaml("/Users/jeremiahomeike/Desktop/ECE346_OsaAnna/ECE346_OsaAnna/ROS_Core/src/Labs/FinalProject/task1.yaml")
-        self.setup_clients()
+        self.truck_x = 0
+        self.truck_y = 0
+        self.goal_positions, self.goal_order = self.parse_yaml("/Users/jeremiahomeike/Desktop/ECE346_OsaAnna/ECE346_OsaAnna/ROS_Core/src/Labs/FinalProject/task1.yaml")
         self.path_topic = get_ros_param('~path_topic', '/Routing/Path')
-        self.odom_topic = get_ros_param('~odom_topic', '/slam_pose')
+        self.odom_topic = get_ros_param('~odom_topic', '/Simulation/Pose')
+        self.setup_clients()
         self.pose_sub = rospy.Subscriber(self.odom_topic, Odometry, self.odometry_callback, queue_size=10)
+
+        threading.Thread(target=self.loop).start()
 
     def odometry_callback(self, odom_msg):
         self.truck_x = odom_msg.pose.pose.position.x
@@ -33,6 +38,9 @@ class Task1_Controller:
             list_of_goals.append(doc["goal_{}".format(idx)])
         
         indexes_of_goals = doc["goal_order"]
+        indexes_of_goals = np.array(indexes_of_goals)
+        indexes_of_goals -= 1 # shift the indexing by one 
+        indexes_of_goals = list(indexes_of_goals)
 
         return list_of_goals, indexes_of_goals
 
@@ -51,29 +59,37 @@ class Task1_Controller:
         plan_request = PlanRequest([x_start, y_start], [x_goal, y_goal])
         plan_response = self.plan_client(plan_request)
         path = plan_response.path
-
+        path.header.stamp = rospy.get_rostime()
+        path.header.frame_id = 'map'
         self.path_pub.publish(path)
+        print("We published the path woot!!!")
     
-    def main_loop(self):
+    def loop(self):
         pointer = 0
+        threshold = .5
+
         while rospy.is_shutdown() is False:
+       ##     if pointer == 0:   
+               # for idx in range(len(self.list_of_goals))         
             x = self.truck_x
             y = self.truck_y
             state = np.array([x, y])
 
-            if np.linalg.norm(state - self.list_of_goals[self.indexes_of_goals[pointer]]) < .5:
+            norm = np.linalg.norm(state - self.goal_positions[self.goal_order[pointer]])
+            print("The norm is ", norm)
+            if norm < threshold:
                 pointer += 1
+                print("Print we're at a new pointer now")
+                rospy.sleep(.5)
                 
             else:
-                self.pub_ref_path(state, self.list_of_goals[self.indexes_of_goals[pointer]])
-                rospy.sleep(1.5)
+                self.pub_ref_path(state, self.goal_positions[self.goal_order[pointer]])
+                print("We're going to ", self.goal_order[pointer] + 1, " for our next state!")
+                rospy.sleep(.5)
 
-            if pointer > len(self.indexes_of_goals) - 1:
-                pointer = 0
-                
-if __name__ == '__main__':
+            if pointer > len(self.goal_order) - 1:
+                break
 
-    ctrl = Task1_Controller()
     
 
 
--- ROS_Core/src/Labs/FinalProject/scripts/task1_waypoint_nav_node.py
+++ ROS_Core/src/Labs/FinalProject/scripts/task1_waypoint_nav_node.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
-from task1_traj_planner.py import TrajectoryPlanner
+from task1_traj_planner import Task1_Controller
 
 if __name__ == '__main__':
-    rospy.init_node('Final Project Task 1 Node')
+    rospy.init_node('task1_waypoint_nav_node')
     rospy.loginfo("Start Final Project Task 1 Node")
-
-    planner = TrajectoryPlanner()
+    Task1_Controller()
     rospy.spin()
